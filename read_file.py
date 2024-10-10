b9f53b01b61e09910814bea5b0a40ee54f0218cf
Yue <janie.zhaoy@gmail.com>
2024-10-10 17:14:36 -0400

    update read_file.py

--- read_file.py
+++ read_file.py
@@ -1,7 +1,7 @@
 ### This Python script reads a .txt file, count things in the file, and output a new txt file
 
 # Read the txt file
-fhand = open('romeo-full.txt')
+fhand = open('exercise-files/romeo-full.txt')
 
 # Define variables to count things
 count = 0 # count lines
@@ -11,10 +11,18 @@ count = 0 # count lines
 for line in fhand:
     count = count + 1
     # more code to count things
+    if line.startswith('ROMEO'):
+        count_Romeo = count_Romeo + 1
+    if line.startswith('JULIET'):
+        count_Juliet = count_Juliet + 1
+
+
 
 # Create an output.txt file
 fout = open('output.txt', 'w')
-fout.write(count)
+fout.write('Line of text: ' + str(count) + '\n')
+fout.write('Romeo: ' + str(count_Romeo) + '\n')
+fout.write('Juliet: ' + str(count_Juliet) + '\n')
 
 # Close the file
 fout.close()
